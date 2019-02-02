eab94ec0df39dc41b9773ff5c9d84324a4704cae
vasanth <vasanth.naidu.90@gmail.com>
2019-02-02 19:53:33 +0530

    created a java file and updated

---
+++ src/Hi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Hi{
+public static void main(String Args[]){
+int a ,b ,c;
+System.out.println("Enter two numbers");
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+c=a+b;
+System.out.println("value of c is"+c);
+}
+}
